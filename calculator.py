883646f2401c30e762ef61d6cfae5e302fe197f0
Safeer hussain <safeerh2005@gmail.com>
2026-05-01 16:00:06 +0500

    Add calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# calculator.py
+def add(a: float, b: float) -> float:
+    return a + b
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
+    print("5 - 1 =", subtract(5, 1))
